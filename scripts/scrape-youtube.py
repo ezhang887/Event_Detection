87fd8aa91cc177f5103e3027b17df46050b344df
Eric Zhang <ezhang887@gmail.com>
2018-10-16 22:49:41 -0500

    update code to automatically make a directory for videos if the specific directory doesn't exist already

--- scripts/scrape-youtube.py
+++ scripts/scrape-youtube.py
@@ -3,10 +3,12 @@
 from pytube import YouTube
 import json
 import argparse
+import os
 
 def download(url, output_path, filename):
     yt = YouTube(url)
-    yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first().download(output_path=output_path, filename=filename)
+    yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution")\
+            .desc().first().download(output_path=output_path, filename=filename)
 
 
 '''
@@ -19,25 +21,29 @@ json config file structure (as of now):
         "video_two_name": {
             "url": "www.example2.com"
         }
-    }
+    },
+    "output_dir": "~/test/example"
 }
 '''
 
 def main():
     parser = argparse.ArgumentParser(description="Scrape videos from Youtube")
     parser.add_argument("json_config_file")
-    parser.add_argument("output_path")
     args = parser.parse_args()
 
     json_file = args.json_config_file
-    output_path = args.output_path
 
     with open(json_file) as f:
         data = json.load(f)
+
+    output_dir = data["output_dir"]
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+
     for name in data["videos"]:
         #maybe there is a cleaner way to do this?
         url = data["videos"][name]["url"]
-        download(url, output_path, name)
+        download(url, output_dir, name)
 
 
 if __name__ == "__main__":
