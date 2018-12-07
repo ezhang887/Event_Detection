dd2f3c340b6b6fb1a6282c1a248e8a5d167628cf
Eric Zhang <ezhang887@gmail.com>
2018-12-06 21:05:25 -0600

    working demo

--- card-detection/detect.py
+++ card-detection/detect.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import time
 
 #abstract class
 class CardDetector:
@@ -9,46 +10,58 @@ class CardDetector:
         self.hsv_test = None
 
     def solidity(self, contour):
-        x,y,w,h = cv2.boundingRect(contour)
-        return cv2.contourArea(contour)/(w*h)
+        w,h = cv2.minAreaRect(contour)[1]
+        area = w*h
+        contourArea = cv2.contourArea(contour)
+        print(contourArea/area)
+        return contourArea/area
 
     def area(self, contour, total_area):
         area = cv2.contourArea(contour)
         return area/total_area
 
-    def detect(self, image):
+    def draw_rotated_rect(self, image, contour):
+        rect = cv2.minAreaRect(contour)
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        cv2.drawContours(image, [box], -1, (255,0,0), 3)
+        return rect
+
+    def detect(self, image, display_time = 5, debug = False):
         height, width, _ = image.shape
         total_area = height*width
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, self.hsv_low, self.hsv_high)
         _, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        good = []
         for i in range(len(contours)):
             if hierarchy[0,i,3] != -1:
                 continue
             cnt = contours[i]
             cnt = cv2.convexHull(cnt)
             rect = cv2.boundingRect(cnt)
-            if self.area(cnt, total_area) >= 1/1000 and self.area(cnt, total_area) <= 1/100 and cv2.isContourConvex(cnt):
-                good.append(cnt)
-        cv2.drawContours(image, good, -1, (255,0,0), 3)
-        while True:
+            x,y,w,h = rect
+            aspect_ratio = h/w
+            if self.area(cnt, total_area) >= 1/1000 and self.area(cnt, total_area) <= 1/100 and cv2.isContourConvex(cnt) and aspect_ratio < 3 and aspect_ratio > 1/3 and self.solidity(cnt) > 0.6:
+                self.draw_rotated_rect(image, cnt)
+        start_time = time.time()
+        while time.time() < start_time + display_time:
             cv2.imshow("image", image)
-            cv2.imshow("hsv", hsv)
-            cv2.imshow("mask", mask)
+            if debug:
+                cv2.imshow("hsv", hsv)
+                cv2.imshow("mask", mask)
             cv2.waitKey(10)
 
 #red card detector class
 class RedCardDetector(CardDetector):
 
     def __init__(self):
-        self.hsv_low = np.array([0,150,150])
+        self.hsv_low = np.array([0,150,200])
         self.hsv_high = np.array([10,255,255])
 
 #yellow card detector class
 class YellowCardDetector(CardDetector):
 
     def __init__(self):
-        self.hsv_low = np.array([20,120,180])
+        self.hsv_low = np.array([20,120,150])
         self.hsv_high = np.array([40,255,255])
 
--- card-detection/main.py
+++ card-detection/main.py
@@ -1,16 +1,29 @@
 from detect import *
 import cv2
+import os
+import glob
 
-'''
-#image = cv2.imread("more.png")
-#image = cv2.imread("other.jpg")
-#image = cv2.imread("image.jpg")
-rc = RedCardDetector()
-rc.detect(image)
-'''
+def main():
+    while True:
+        detector = YellowCardDetector()
+        os.chdir("./yellow")
+        for f in glob.glob("*.jpg"):
+            detector.detect(cv2.imread(f), 3)
+        for f in glob.glob("*.png"):
+            detector.detect(cv2.imread(f), 3)
+        detector = RedCardDetector()
+        os.chdir("../red")
+        for f in glob.glob("*.jpg"):
+            detector.detect(cv2.imread(f), 3)
+        for f in glob.glob("*.png"):
+            detector.detect(cv2.imread(f), 3)
+        os.chdir("../")
 
-image = cv2.imread("yellow_two.jpg")
 
-yc = YellowCardDetector()
-yc.detect(image)
+def test():
+    detector = RedCardDetector()
+    detector.detect(cv2.imread("red/1.jpg"), 10000, True)
 
+
+if __name__ == "__main__":
+    main()
